8c6a15f88c0681d7b8d7bfb528fc4dd35d5e66a5
Joshua Byrd <phocks@gmail.com>
2023-01-26 21:02:32 +1000

    unique_key add to username

--- migration/src/m20220101_000003_create_users_table.rs
+++ migration/src/m20220101_000003_create_users_table.rs
@@ -22,7 +22,7 @@ impl MigrationTrait for Migration {
             .auto_increment()
             .primary_key()
         )
-        .col(ColumnDef::new(Users::Username).string().not_null())
+        .col(ColumnDef::new(Users::Username).string().unique_key().not_null())
         .col(ColumnDef::new(Users::PasswordHash).string().not_null())
         .col(ColumnDef::new(Users::Email).string())
         .to_owned()
