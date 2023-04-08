5f63e5fa2e165c2ba7e41a7e662c69810ece2562
Joshua Byrd <phocks@gmail.com>
2023-04-08 23:12:26 +1000

    Did something with graphql

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,27 @@
 mod entities;
 mod tokens;
 mod database;
+mod schema;
 
 // Actix web imports
 use actix_cors::Cors;
-use actix_web::{ web, App, HttpServer };
+use actix_web::{guard, web, web::Data, App, HttpResponse, HttpServer, Result};
+use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema};
+use async_graphql_actix_web::*;
+use schema::*;
+
+type SchemaType = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
 
 // Load our API routes
 mod routes;
 
 use dotenv::dotenv;
+
+async fn index_graphiql() -> Result<HttpResponse> {
+  Ok(HttpResponse::Ok()
+      .content_type("text/html; charset=utf-8")
+      .body(GraphiQLSource::build().endpoint("/").finish()))
+}
 
 #[macro_use]
 extern crate dotenv_codegen;
@@ -33,6 +45,7 @@ async fn main() -> std::io::Result<()> {
           ::scope("/v1")
           .service(routes::user::login)
           .service(routes::user::register)
+          .service(web::resource("/").guard(guard::Get()).to(index_graphiql))
       )
   })
     .bind(("0.0.0.0", 3000))?
