50f47e3856a90011fd974255ffe96c28ba8340e6
Joshua Byrd <phocks@gmail.com>
2023-04-10 21:59:25 +1000

    Add test graphql

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@
 mod entities;
 mod tokens;
 mod database;
-mod schema;
+// mod schema;
+mod star_wars;
+
+// Load our API routes
+mod routes;
 
 // Actix web imports
 use actix_cors::Cors;
@@ -15,24 +19,29 @@ use async_graphql::{
   EmptySubscription,
   Schema,
 };
-use async_graphql_actix_web::*;
-use schema::*;
+use async_graphql_actix_web::{ GraphQLRequest, GraphQLResponse };
+// use schema::*;
+use star_wars::{ QueryRoot, StarWars, StarWarsSchema };
 
-type SchemaType = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
-
-// Load our API routes
-mod routes;
+// type SchemaType = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
 
 use dotenv::dotenv;
 
 #[macro_use]
 extern crate dotenv_codegen;
 
+async fn index(
+  schema: web::Data<StarWarsSchema>,
+  req: GraphQLRequest
+) -> GraphQLResponse {
+  schema.execute(req.into_inner()).await.into()
+}
+
 async fn index_graphiql() -> Result<HttpResponse> {
   Ok(
     HttpResponse::Ok()
       .content_type("text/html; charset=utf-8")
-      .body(GraphiQLSource::build().endpoint("/").finish())
+      .body(GraphiQLSource::build().endpoint("/graphql").finish())
   )
 }
 
@@ -41,20 +50,75 @@ async fn main() -> std::io::Result<()> {
   // Load local environment variables from .env file
   dotenv().ok();
 
+  let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
+    .data(StarWars::new())
+    .finish();
+
   println!("Attempting to listen on http://localhost:3000");
 
-  HttpServer::new(|| {
+  HttpServer::new(move || {
     App::new()
       .wrap(Cors::permissive())
+      .app_data(Data::new(schema.clone()))
       .service(routes::home)
+      .service(web::resource("/graphql").guard(guard::Post()).to(index))
+      .service(web::resource("/graphql").guard(guard::Get()).to(index_graphiql))
       .service(
         web
           ::scope("/v1")
           .service(routes::user::login)
           .service(routes::user::register)
-          .service(web::resource("/").guard(guard::Get()).to(index_graphiql))
       )
   })
+    // App::new()
+    //   .app_data(Data::new(schema.clone()))
+    //   .wrap(Cors::permissive())
+    //   .service(routes::home)
+    //   .service(
+    //     web
+    //       ::scope("/v1")
+    //       .service(routes::user::login)
+    //       .service(routes::user::register)
+    //       .service(web::resource("/").guard(guard::Post()).to(index))
+    //       .service(web::resource("/").guard(guard::Get()).to(index_graphiql))
+    //   )
+    // })
     .bind(("0.0.0.0", 3000))?
     .run().await
 }
+
+// mod star_wars;
+
+// use actix_web::{guard, web, web::Data, App, HttpResponse, HttpServer, Result};
+// use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema};
+// use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
+// use star_wars::{QueryRoot, StarWars, StarWarsSchema};
+
+// async fn index(schema: web::Data<StarWarsSchema>, req: GraphQLRequest) -> GraphQLResponse {
+//     schema.execute(req.into_inner()).await.into()
+// }
+
+// async fn index_graphiql() -> Result<HttpResponse> {
+//     Ok(HttpResponse::Ok()
+//         .content_type("text/html; charset=utf-8")
+//         .body(GraphiQLSource::build().endpoint("/").finish()))
+// }
+
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
+//         .data(StarWars::new())
+//         .finish();
+
+//     println!("GraphiQL IDE: http://localhost:8000");
+
+//     HttpServer::new(move || {
+//         App::new()
+//             .app_data(Data::new(schema.clone()))
+//             .service(web::resource("/").guard(guard::Post()).to(index))
+//             .service(web::resource("/").guard(guard::Get()).to(index_graphiql))
+//     })
+//     .bind("127.0.0.1:8000")?
+//     .run()
+//     .await
+// }
--- src/schema.rs
+++ src/schema.rs
@@ -1,72 +1,12 @@
-use juniper::{ EmptySubscription, FieldResult, RootNode };
 
-#[derive(GraphQLEnum)]
-enum Episode {
-  NewHope,
-  Empire,
-  Jedi,
-}
 
-use juniper::{ GraphQLEnum, GraphQLInputObject, GraphQLObject };
+use async_graphql::Object;
 
-#[derive(GraphQLObject)]
-#[graphql(description = "A humanoid creature in the Star Wars universe")]
-struct Human {
-  id: String,
-  name: String,
-  appears_in: Vec<Episode>,
-  home_planet: String,
-}
+pub(crate) struct QueryRoot;
 
-#[derive(GraphQLInputObject)]
-#[graphql(description = "A humanoid creature in the Star Wars universe")]
-struct NewHuman {
-  name: String,
-  appears_in: Vec<Episode>,
-  home_planet: String,
-}
-
-pub struct QueryRoot;
-
-#[juniper::graphql_object]
+#[Object]
 impl QueryRoot {
-  fn human(_id: String) -> FieldResult<Human> {
-    Ok(Human {
-      id: "1234".to_owned(),
-      name: "Luke".to_owned(),
-      appears_in: vec![Episode::NewHope],
-      home_planet: "Mars".to_owned(),
-    })
-  }
+    async fn hello(&self) -> String {
+        "Hello GraphQL".to_owned()
+    }
 }
-
-pub struct MutationRoot;
-
-#[juniper::graphql_object]
-impl MutationRoot {
-  fn create_human(new_human: NewHuman) -> FieldResult<Human> {
-    Ok(Human {
-      id: "1234".to_owned(),
-      name: new_human.name,
-      appears_in: new_human.appears_in,
-      home_planet: new_human.home_planet,
-    })
-  }
-}
-
-pub type Schema = RootNode<'static, QueryRoot, MutationRoot, EmptySubscription>;
-
-pub fn create_schema() -> Schema {
-  Schema::new(QueryRoot {}, MutationRoot {}, EmptySubscription::new())
-}
-
-// use async_graphql::Object;
-
-// pub(crate) struct QueryRoot;
-
-// #[Object]
-// impl QueryRoot {
-//     async fn hello(&self) -> String {
-//         "Hello GraphQL".to_owned()
-//     }
-// }
